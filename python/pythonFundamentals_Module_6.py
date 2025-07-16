264d984c92a663485300bb87cb0c0ac1491de3e8
Billy Beck <billys.beck@gmail.com>
2025-07-15 21:11:57 -0400

    python assignment

---
+++ python/pythonFundamentals_Module_6.py
@@ -0,0 +1,4 @@
+c = "cats"; #Set variable c to "cats."
+d = "dogs"; #Set variable d to "dogs."
+s = "It's raining " + c + " and " + d + "."; #Set variable s to the sentence to print
+print(s); """This prints the entire message."""
